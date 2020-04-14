19241522bd9a0b6fa36acc7f7b05a94d847ee190
JohnPolvorosa <johnpolvorosa@ontariotechu.net>
2020-04-14 02:36:45 -0400

    added milk function

--- index.js
+++ index.js
@@ -12,6 +12,36 @@
 // var input1 = input.slice(0,1);
 // alert("Hello " + input1.toUpperCase() + (input.slice(1,input.length)).toLowerCase());
 
-var dogAge = prompt("Enter your Dog Age: ");
-var humanAge = (dogAge-2)*4 + 21;
-alert("Your dog is " + humanAge + " in human age");
+function calcDogAge() {
+    var dogAge = prompt("Enter your Dog Age: ");
+    var humanAge = (dogAge-2)*4 + 21;
+    alert("Your dog is " + humanAge + " in human age");
+}
+
+// calcDogAge();
+
+function getMilk(money) {
+    var bucketCost = 2   
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("bucket of milk costs: " + bucketCost);
+  console.log("bought " + Math.floor(money/bucketCost) + " buckets of milk");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk(69);
