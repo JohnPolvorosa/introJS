24d4ec9ea03871d336728976a14cf0c1bfcbb15b
JohnPolvorosa <johnpolvorosa@ontariotechu.net>
2020-04-14 23:54:53 -0400

    reduced lines fib

--- fibonacci.js
+++ fibonacci.js
@@ -1,18 +1,14 @@
 function fibonacciGenerator (n) {
-    //Do NOT change any of the code above 👆
-        
-        //Write your code here:
+
         let sequence = [];
         let sum, count = 0;
         
         if (n <= 1) {
             sequence.push(0);
         } else if (n === 2) {
-            sequence.push(0);
-            sequence.push(1);
+            sequence.push(0,1);
         } else {
-            sequence.push(0);
-            sequence.push(1);
+            sequence.push(0,1);
             count = 2;
             while (count < n) {
                 sum = ((sequence[count-1]) + (sequence[count-2]));
@@ -21,11 +17,6 @@ function fibonacciGenerator (n) {
             }
         }
         return sequence;
-        
-        //Return an array of fibonacci numbers starting from 0.
-    //     return sequence;
-        
-    //Do NOT change any of the code below 👇
     }
     
     
